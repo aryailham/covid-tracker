765568d6701e003f5676ff7510b48312d8687785
Arya <aryailham05@users.github.com>
2020-09-10 22:29:10 +0700

    Update Layout.js

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from "./Layout.module.css";
 const layout = (props) => {
   return (
     <React.Fragment>
-      <div>header</div>
+      {/* <div>header</div> */}
       <main className={classes.Main}>{props.children}</main>
     </React.Fragment>
   );
